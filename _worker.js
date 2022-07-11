c515ea02429693dba8d8595bb35b18833f656916
kklmn13 <cyan_13@foxmail.com>
2022-07-11 16:23:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="220.160.10.31.sslip.io:8880";
+        url.hostname="cyanse.ml";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
