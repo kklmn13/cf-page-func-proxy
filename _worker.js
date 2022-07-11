0d1517cc56c1ac69475cd0f323e896840ab43693
kklmn13 <cyan_13@foxmail.com>
2022-07-11 16:35:44 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="baidu.com";
+        url.hostname="lsq666.tk";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
